da8f69b304117cd97d260dc909a2f6162332231d
James Brown <soylentblue@hotmail.co.uk>
2022-09-20 20:44:26 +1000

    modify ethCall for ENS library

--- core/src/main/java/org/web3j/protocol/core/methods/response/EthCall.java
+++ core/src/main/java/org/web3j/protocol/core/methods/response/EthCall.java
@@ -6,7 +6,19 @@ import org.web3j.protocol.core.Response;
  * eth_call.
  */
 public class EthCall extends Response<String> {
+
+    // Numeric.toHexString(Hash.sha3("Error(string)".getBytes())).substring(0, 10)
+    private static final String errorMethodId = "0x08c379a0";
+
     public String getValue() {
-        return getResult();
+        return (String)this.getResult();
+    }
+
+    public boolean isReverted() {
+        return this.hasError() || this.isErrorInResult();
+    }
+
+    private boolean isErrorInResult() {
+        return this.getValue() != null && this.getValue().startsWith("0x08c379a0");
     }
 }
